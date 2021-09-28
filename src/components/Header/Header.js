abc631bf4f9730a2b385bf43e4ed627e78fa81d9
econom1st <camcycamovu4@gmail.com>
2021-09-28 09:30:36 +0300

    add link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Header.scss';
 
+import { Link } from "react-router-dom";
+
+
 const Header = () => {
     return (
         <div className="header">
-            <button className="header__btn">Valutes</button>
-            <button className="header__btn">News</button>
+            <Link to='/valutes'><button className="header__btn">Valutes</button></Link>
+            <Link to='/#'><button className="header__btn">News</button></Link>
         </div>
     );
 };
