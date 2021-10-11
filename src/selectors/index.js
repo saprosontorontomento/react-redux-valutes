b9afe6a1f6d5430f82757c91835dfaece7fdd33a
econom1st <camcycamovu4@gmail.com>
2021-10-11 17:58:30 +0300

    news selector

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,2 +1,3 @@
 export const valutesSelector = (state) => state.valutes.valutes; //  мост между state и components . Так мне нужно из state что-то и отдает some
 export const dateSelector = (state) => state.valutes;
+export const newsSelector = (state) => state;
