591c1aaac2098c55b4e64ce9a266aa9517589611
econom1st <camcycamovu4@gmail.com>
2021-10-11 17:57:38 +0300

    ref code

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import valutesReducer from './valutesReducer';
+import newsReducer from './newsReducer';
 
 export default combineReducers({ // модули. Неймниг в него прокидываешь редюсеры
   valutes: valutesReducer,
+  news: newsReducer,
   
 });
