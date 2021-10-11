3e9d7c582d474394b3433eb8ab814c5fb8975fdb
econom1st <camcycamovu4@gmail.com>
2021-10-11 09:44:16 +0300

    ref code

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,26 +1,15 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 
-import { getValutes } from '../../actions';
-import { valutesSelector, dateSelector } from '../../selectors';
 
 import Header from '../Header/Header';
 import Valutes from '../Valutes/Valutes';
 import './Main.scss'
 
 const Main = () => {
-    const dispatch = useDispatch();
-    const valutes =  useSelector(valutesSelector);
-    const date = useSelector(dateSelector);
+    // const dispatch = useDispatch();
+    // const valutes =  useSelector(valutesSelector);
+    // const date = useSelector(dateSelector);
 
-    
-    console.log(valutes);
-    console.log(date);
-
-    useEffect(() => {
-        dispatch(getValutes())
-        
-    }, [dispatch])
 
   
     return (
