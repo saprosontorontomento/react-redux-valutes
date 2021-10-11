f7e9b1f99c6316dfc0d17d112b6cc353f9449467
econom1st <camcycamovu4@gmail.com>
2021-10-11 09:43:13 +0300

    ref code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import './App.scss';
+
 import Main from '../Main/Main';
 import Header from '../Header/Header';
+import News from '../News/News';
 
 
 
 const App = () => {
   return (
       <BrowserRouter>
-        <div>
+        <div className="app">
           <Switch>
             <Route exact path="/" component={Header} />
+            <Route  path="/news" component={News} />
             <Route  path="/valutes" component={Main} />
           </Switch>
         </div>
