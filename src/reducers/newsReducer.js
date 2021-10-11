4f7e681e2b406447aac62ec003a2e988ffe3e3b3
econom1st <camcycamovu4@gmail.com>
2021-10-11 17:57:15 +0300

    add news reducer

---
+++ src/reducers/newsReducer.js
@@ -0,0 +1,17 @@
+import { ADD_NEWS } from "../actions/actionTypes";
+
+const initialState = { // что в нём есть или будет. ЧТО ПРИМИТЬСЯ
+    news: {}
+};
+
+export default function news(state = initialState, action) { // могу поменять данные с запроса, фильтровать. Раскинули и обработали запрос
+    switch (action.type) {
+        case ADD_NEWS:
+            return {
+                ...state,
+                news: action.payload.news,
+            }
+        default:
+            return state;
+    }
+}
