f7285dd095e65ced7f18e9126c1999dbb9b0af56
econom1st <camcycamovu4@gmail.com>
2021-10-15 15:44:48 +0300

    ref code

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,3 +1,2 @@
 export const valutesSelector = (state) => state.valutes.valutes; //  мост между state и components . Так мне нужно из state что-то и отдает some
-export const dateSelector = (state) => state.valutes;
 export const newsSelector = (state) => state;
